af3f81a40f102f9c1996d9d5663633586162e187
Eduardo Sanches <edupsanches@gmail.com>
2024-11-09 11:46:59 -0300

    Setting up guardians fieldset

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// const cellphoneNumber = document.getElementById('cellphone')
+
+// cellphoneNumber.addEventListener('input', () => {
+//     const cellphoneRegex = /^\(\d{2}\) \d{5}-\d{4}$/
+//     cellphoneNumber = cellphoneRegex.replace()
+// })
